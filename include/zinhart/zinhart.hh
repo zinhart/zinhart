4a096986c601363238c4f5bd5aac2de0824da702
zinhart <aromzinhartdegraca@gmail.com>
2018-11-14 10:28:45 -0800

    Added bmp changed build scripts, Update ann

--- include/zinhart/zinhart.hh
+++ include/zinhart/zinhart.hh
@@ -1,7 +1,8 @@
 #ifndef ZINHART_HH
 #define ZINHART_HH
-#include <zinhart/bmp_parser/bmp_parser.hh>
 #include <multi_core/multi_core.hh>
+#include <bmp/bmp.hh>
+#include <ann/ann.hh>
 #include <cstdint>
 namespace zinhart
 {
